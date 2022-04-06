8a6dd060104e6f84ce298f996b03c10d0299831b
eletonia <elentonia@gmail.com>
2022-04-06 18:51:36 +0000

    changing paths in launch file

--- infer-medium.sh
+++ infer-medium.sh
@@ -26,18 +26,17 @@ echo "run pytorch ..."
 export GPU_ID=4 #choose a device id 
 set -x
 CUDA_VISIBLE_DEVICES=$GPU_ID
-nsys profile -f true -o timeline -c cudaProfilerApi --stop-on-range-end true -s none --gpu-metrics-device=$GPU_ID --gpu-metrics-set=ga100\
- $dlrm_pt_bin --arch-sparse-feature-size=16 \
+$dlrm_pt_bin --arch-sparse-feature-size=16 \
 	--arch-mlp-bot="13-512-256-64-16" \
 	--arch-mlp-top="512-256-1" \
 	--data-generation=dataset \
 	--data-set=kaggle \
-	--raw-data-file=/mnt/nvme1n1p1/DLRM/data-medium/train.txt \
-	--processed-data-file=/mnt/nvme1n1p1/DLRM/data-medium/kaggleAdDisplayChallenge_processed.npz \
+	--raw-data-file=/tmp/dlrm_rd/train.txt \
+	--processed-data-file=/tmp/dlrm_rd/kaggleAdDisplayChallenge_processed.npz \
 	--inference-only \
 	--loss-function=bce \
 	--round-targets=True \
-	--load-model=/mnt/nvme1n1p1/DLRM/data-medium/criteo-medium-100bat.pt \
+	--load-model=/tmp/dlrm_rd/criteo-medium-100bat.pt \
 	--print-freq=1024 \
 	--print-time \
 	--test-mini-batch-size=16384 \
